c1e927a156686acdff5291c99570537a562b9a29
Clelys22 <clelia-spiga-c.s@virgilio.it>
2025-11-20 11:57:28 +0100

    Aggiunta soluzione point aggiornata

--- 2/point/Soluzione_point.ml
+++ 2/point/Soluzione_point.ml
@@ -14,3 +14,33 @@ let p1 =
 }
 
 let translate (p:point) (px : float) (py: float) = { x = p.x +. px; y = p.y +. py};;
+
+let distance_from_origin (p : point) = sqrt(p.x *. p.x +. p.y *. p.y);;
+
+type color = Red | Green | Blue;;
+
+type colored_point = 
+{
+   position : point;
+   color : color;
+}
+
+let cp1 =
+{
+    position = p1;
+    color = Red;
+}
+
+let cp2 =
+{
+    position = p1;
+    color = Green;
+}
+
+let same_color (cp1 : colored_point) ( cp2 : colored_point) = if cp1.color = cp2.color then true else false;;
+
+let recolor (cp : colored_point) (c : color) = {position = cp.position; color = c};;
+
+assert(same_color cp1 cp1 = false);;
+
+
